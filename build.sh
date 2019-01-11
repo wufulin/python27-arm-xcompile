00ef2eebdfa0a39cfa56754f95ea034d5a7b50a5
吴富林 <wfl@eie.ltd>
2019-01-11 11:39:30 +0800

    功能：修改合适自己的交叉编译python27脚本

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # change these to match your environment
-TARGET_HOST="arm-unknown-linux-gnueabi"
-CROSS_TOOLS_PATH=~/arm-toolchain/tools/$TARGET_HOST/bin
-BUILD_HOST="x86_64-linux-gnu"
+TARGET_HOST="arm-fsl-linux-gnueabi"
+CROSS_TOOLS_PATH=/opt/gcc-4.4.4-glibc-2.11.1-multilib-1.0/$TARGET_HOST/bin
+BUILD_HOST="arm"
 
 # you shouldn't need to change these
 PYTHON="Python-2.7.3"
@@ -46,18 +46,34 @@ mv Parser/pgen Parser/hostpgen
 make distclean
 
 # set up environment for cross compile - we really shouldn't blindly add to PATH
+# PATH=$PATH:/opt/gcc-4.4.4-glibc-2.11.1-multilib-1.0/arm-fsl-linux-gnueabi/bin
+# CC=/opt/gcc-4.4.4-glibc-2.11.1-multilib-1.0/arm-fsl-linux-gnueabi/bin/arm-fsl-linux-gnueabi-gcc
+# AR=/opt/gcc-4.4.4-glibc-2.11.1-multilib-1.0/arm-fsl-linux-gnueabi/bin/arm-fsl-linux-gnueabi-ar
+# RANLIB=/opt/gcc-4.4.4-glibc-2.11.1-multilib-1.0/arm-fsl-linux-gnueabi/bin/arm-fsl-linux-gnueabi-ranlib
+# LD=/opt/gcc-4.4.4-glibc-2.11.1-multilib-1.0/arm-fsl-linux-gnueabi/bin/arm-fsl-linux-gnueabi-ld
+
 export PATH="$PATH:$CROSS_TOOLS_PATH"
-export CROSS_COMPILE=arm-unknown-linux-gnueabi-
+export CROSS_COMPILE=arm-fsl-linux-gnueabi-
+export CC=$CROSS_TOOLS_PATH/arm-fsl-linux-gnueabi-gcc
+export AR=$CROSS_TOOLS_PATH/arm-fsl-linux-gnueabi-ar
+export RANLIB=$CROSS_TOOLS_PATH/arm-fsl-linux-gnueabi-ranlib
+export LD=$CROSS_TOOLS_PATH/arm-fsl-linux-gnueabi-ld
+
 
 echo "Stage 1.5: patching Python for cross-compile .."
 patch -p0 < ../files/Python-2.7.3-xcompile.patch
 
 # cross compile
 echo "Stage 2: cross-compiling for $TARGET_HOST .."
+# ./configure --disable-ipv6 --build="arm" --host="arm-fsl-linux-gnueabi" --prefix=$PWD/_install LDFLAGS="-L/usr/lib  -static -static-libgcc" CPPFLAGS="-I/usr/include -static" CONFIG_SITE="config.site"
 ./configure $CONFIGURE_ARGS --build=$BUILD_HOST --host=$TARGET_HOST \
-    LDFLAGS="-static -static-libgcc" CPPFLAGS="-static" CONFIG_SITE="config.site" >> $BUILD_LOG
+    LDFLAGS="-L/usr/lib -static -static-libgcc" CPPFLAGS="-I/usr/include -static" CONFIG_SITE="config.site" >> $BUILD_LOG
 sed -i '1r ../files/Setup' Modules/Setup
-make HOSTPYTHON=./hostpython HOSTPGEN=./Parser/hostpgen CROSS_COMPILE_TARGET=yes BUILDARCH=$BUILD_HOST HOSTARCH=$TARGET_HOST >> $BUILD_LOG
+# make HOSTPYTHON=./hostpython HOSTPGEN=./Parser/hostpgen CROSS_COMPILE_TARGET=yes BLDSHARED="arm-fsl-linux-gnueabi-gcc -shared" BUILDARCH="arm" HOSTARCH="arm-fsl-linux-gnueabi"
+make HOSTPYTHON=./hostpython HOSTPGEN=./Parser/hostpgen BLDSHARED="arm-fsl-linux-gnueabi-gcc -shared" CROSS_COMPILE_TARGET=yes BUILDARCH=$BUILD_HOST HOSTARCH=$TARGET_HOST >> $BUILD_LOG
+
+# make install HOSTPYTHON=./hostpython BLDSHARED="arm-fsl-linux-gnueabi-gcc -shared" CROSS_COMPILE=arm-fsl-linux-gnueabi- CROSS_COMPILE_TARGET=yes prefix=$PWD/_install
+make install HOSTPYTHON=./hostpython BLDSHARED="arm-fsl-linux-gnueabi-gcc -shared" CROSS_COMPILE=$CROSS_COMPILE CROSS_COMPILE_TARGET=yes prefix=$PWD/_install >> $BUILD_LOG
 
 sed -n -e '/Python build finished/,$p' $BUILD_LOG | grep -v 'install'
 file python
